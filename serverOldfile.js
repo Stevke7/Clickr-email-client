06b1b9df5b5e2e5d8344d0a17dce712e1a1ad477
Ljubisa Stevanovic <stevanovicljubisa07@gmail.com>
2025-02-15 19:23:01 +0100

    making an serverless function

---
+++ serverOldfile.js
@@ -0,0 +1,64 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const app = express();
+
+// // CORS configuration (Essential!)
+// const allowedOrigins = [process.env.ALLOWED_ORIGIN]; // Get allowed origin from env variable
+
+// if (!allowedOrigins[0]) {
+// 	console.error("ALLOWED_ORIGIN environment variable is not set!");
+// 	process.exit(1); // Exit if the variable isn't set
+// }
+
+// app.use(
+// 	cors({
+// 		origin: function (origin, callback) {
+// 			if (!origin || allowedOrigins.includes(origin)) {
+// 				// Allow requests with no origin (like Postman)
+// 				callback(null, true);
+// 			} else {
+// 				callback(new Error("Not allowed by CORS"));
+// 			}
+// 		},
+// 	})
+// );
+
+// //Middleware
+// app.use(bodyParser.json());
+
+// app.post("/send-email", async (req, res) => {
+// 	const { name, email, message } = req.body;
+
+// 	try {
+// 		const transporter = nodemailer.createTransport({
+// 			service: "Gmail",
+// 			auth: {
+// 				user: process.env.USER,
+// 				pass: process.env.PASS,
+// 			},
+// 		});
+
+// 		const mailOptions = {
+// 			from: email,
+// 			to: process.env.USER,
+// 			subject: `Message from ${name}`,
+// 			text: message,
+// 			replyTo: email,
+// 			subject: `Message from ${name}`,
+// 			text: `You have received a message from ${name} (${email}):\n\n${message}`,
+// 		};
+
+// 		await transporter.sendMail(mailOptions);
+// 		res.status(200).json({ message: "Email sent successfully!" }); // Send JSON response
+// 	} catch (error) {
+// 		console.error(error);
+// 		res.status(500).json({ error: "Failed to send email." }); // Send JSON error response
+// 	}
+// });
+
+// const PORT = process.env.PORT || 3001; // Use environment variable for port
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
