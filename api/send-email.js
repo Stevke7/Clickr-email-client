33ddfbb355c58e8b97ea2f5a40e14accb020dae5
Ljubisa Stevanovic <stevanovicljubisa07@gmail.com>
2025-02-21 20:18:08 +0100

    new changes

--- api/send-email.js
+++ api/send-email.js
@@ -1,13 +1,29 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 module.exports = async (req, res) => {
+	// Set CORS headers
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
+
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
+
 	if (req.method !== "POST") {
 		return res.status(405).json({ error: "Method not allowed" });
 	}
 
 	const { name, email, message } = req.body;
-	//comments
+
 	try {
 		const transporter = nodemailer.createTransport({
 			service: "Gmail",
