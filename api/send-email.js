d69cd2b2efb12bd40814cc480d2cf08a8e60fac2
Ljubisa Stevanovic <stevanovicljubisa07@gmail.com>
2025-02-21 20:43:11 +0100

    allow only from clickr.studio

--- api/send-email.js
+++ api/send-email.js
@@ -2,16 +2,18 @@ const nodemailer = require("nodemailer");
 
 module.exports = async (req, res) => {
 	// Set CORS headers
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
-	);
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-	);
+	const origin = req.headers.origin;
+	if (origin === "https://clickr.studio") {
+		res.setHeader("Access-Control-Allow-Origin", origin);
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+		);
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+		);
+	}
 
 	if (req.method === "OPTIONS") {
 		res.status(200).end();
